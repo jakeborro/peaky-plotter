30571ba0dcd8849a6445aaa258e347cd7a29970a
Jake Borro <jakegborro@gmail.com>
2015-07-08 11:18:28 -0700

    Update plotr.py

--- plotr.py
+++ plotr.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import datetime as DT
 import plotly.plotly as py
 
+#Creates a list of tuples from .csv file
 data= np.loadtxt('daily_count.csv', delimiter=',',
          dtype={'names': ('date', 'time'),'formats': ('S10', 'S10')} )
 
+#Zips files to parse data
 dates, times = map(list, zip(*data))
 print dates, times
 
@@ -22,7 +24,7 @@ ax.grid()
 plt.plot(x,y)
 plt.xlabel('Date')
 plt.ylabel('Time')
-plt.title('Peak Time')
+plt.title('Peak Times')
 
 
 
