a5dc54b383e3e49fa701c68938a2e93de186ef44
Jake Borro <jakegborro@gmail.com>
2015-07-14 17:48:20 -0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,11 +1,8 @@
 import csv
 
-remove_from = 2
-remove_to = 5
-
-with open("data.csv", "rb") as fp_in, open("data2.csv", "wb") as fp_out:
-    reader = csv.reader(fp_in, delimiter=",")
-    writer = csv.writer(fp_out, delimiter=",")
-    for row in reader:
-        del row[remove_from:remove_to]
-        writer.writerow(row)
+with open("data.csv","rb") as source:
+    readr = csv.reader(source)
+    with open("data2.csv","wb") as result:
+        writr = csv.writer(result)
+        for r in readr:
+            writr.writerow((r[2], r[4]))
