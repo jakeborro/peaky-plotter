b53d35b21c6c5fac799cc8298dbdb2f202d568ae
Jake Borro <jakegborro@gmail.com>
2015-07-29 21:30:45 -0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,9 +1,13 @@
 import csv
+import os
+import glob
 
-#Extracts the important columns from the .csv into a new file
-with open("data.csv","rb") as source:
-    readr = csv.reader(source)
-    with open("data2.csv","wb") as result:
-        writr = csv.writer(result)
-        for r in readr:
-            writr.writerow((r[2], r[4]))
+path = '/path/to/file'
+for infile in glob.glob( os.path.join(path, '*.history.csv') ):
+    output = infile + '.out'
+    with open(infile, 'r') as source:
+        readr = csv.reader(source)
+        with open(output,"w") as result:
+            writr = csv.writer(result)
+            for r in readr:
+                writr.writerow((r[4], r[2]))
