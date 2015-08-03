7206cba5ae70de91665b8020ef6b14fe074e5256
Jake Borro <jakegborro@gmail.com>
2015-08-03 15:05:56 -0700

    Update plotr.py

--- plotr.py
+++ plotr.py
@@ -1,33 +1,40 @@
+#!/usr/bin/python
+
 import numpy as np
 from pylab import *
 import matplotlib.pyplot as plt
 import datetime as DT
 import matplotlib.dates as mdates
-import scipy
-
-data= np.loadtxt('data2.csv', delimiter=',',
-         dtype={'names': ('date', 'session'),'formats': ('S10', 'i4')} )
-
-#Organizes 2-column spreadsheet
-dates, sessions = map(list, zip(*data))
-print dates, sessions
-
-x = [DT.datetime.strptime(date,"%m-%d-%y") for date in dates]
-y = [sessions]
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.xaxis_date()
-ax.grid()
-#Fills space under plotted line
-ax.fill_between(x, sessions, color='blue')
-
-# slants the x axis
-fig.autofmt_xdate()
-plt.plot(x,sessions)
-plt.xlabel('Date')
-plt.ylabel('Sessions')
-plt.title('Peak Usage')
+import glob
 
 
-savefig('graph.png')
+def main():
+  files = glob.glob('./*.history.csv')
+
+  for file in files:
+    data = np.loadtxt(file, delimiter=',', dtype={'names': ('date', 'session'),'formats': ('S10', 'i4')} )
+
+    #Organizes 2-column spreadsheet
+    dates, sessions = map(list, zip(*data))
+    print dates, sessions
+
+    x = [DT.datetime.strptime(date,"%m-%d-%y") for date in dates]
+    y = [sessions]
+
+    fig = plt.figure()
+    axis = fig.add_subplot(111)
+    axis.xaxis_date()
+    axis.grid()
+    #Fills space under plotted line
+    axis.fill_between(x, sessions, color='blue')
+
+    # slants the x axis
+    fig.autofmt_xdate()
+    plt.plot(x,sessions)
+    plt.xlabel('Date')
+    plt.ylabel('Sessions')
+    plt.title('Peak Usage')
+
+    savefig(file + '.png')
+
+main()
