56abd6fb49748af6d2ca82c952cb857ac4110806
Jake Borro <jakegborro@gmail.com>
2015-08-04 13:36:18 -0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -1,13 +1,14 @@
 import csv
-import os
 import glob
 
-path = '/path/to/file'
-for infile in glob.glob( os.path.join(path, '*.history.csv') ):
-    output = infile + '.out'
-    with open(infile, 'r') as source:
-        readr = csv.reader(source)
-        with open(output,"w") as result:
-            writr = csv.writer(result)
-            for r in readr:
-                writr.writerow((r[4], r[2]))
+def main():
+    files = glob.glob('./*.history')
+
+    for file in files:
+        with open(file,"rb") as source:
+            readr = csv.reader(source)
+            with open(file + '.csv' ,"wb") as result:
+                writr = csv.writer(result)
+                for r in readr:
+                    writr.writerow((r[4], r[2]))
+main()
