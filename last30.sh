a1a0f68f4ef054e46c46a3d7e69347b7d50f5a94
Jake Borro <jakegborro@gmail.com>
2015-08-04 13:37:48 -0700

    Update last30.sh

--- last30.sh
+++ last30.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 find . -type f \( -name "*.history.csv" \) -exec tail -n 32 "$file" {} + > "$file"extract.csv
 
-awk '!/==>/' extract.csv > temp && mv temp extracted.csv
+#awk '!/==>/' extract.csv > temp && mv temp extracted.csv
 
-awk -F "," '{print $0 >> ("file" $1 ".history.csv")}' extracted.csv
+#awk -F "," '{print $0 >> ("file" $1 ".history.csv")}' extracted.csv
